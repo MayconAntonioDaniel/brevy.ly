fd3258ac98a5a091b0cc38dcc99e46836089e028
Maycon Antônio Daniel <mayconantonio1997@hotmail.com>
2026-02-18 20:31:33 -0300

    Documentation with swagger

--- src/infra/http/routes/create-link.ts
+++ src/infra/http/routes/create-link.ts
@@ -1,7 +1,23 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 export const createLinkRoute: FastifyPluginAsyncZod = async server => {
-  server.post('/links', () => {
-    return 'Hello World'
-  })
+  server.post(
+    '/links',
+    {
+      schema: {
+        summary: 'Create a new shortened link',
+        body: z.object({
+          url: z.string(),
+        }),
+        response: {
+          201: z.object({ urlId: z.string() }),
+          409: z.object({ message: z.string() }).describe('Url already exists'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ urlId: 'abc123' })
+    }
+  )
 }
